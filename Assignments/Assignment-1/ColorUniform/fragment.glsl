235f16ac9dc024f8249a721d61f251fb7db2ddeb
nandukalidindi <nandukalidindi@gmail.com>
2016-09-27 00:34:24 -0400

    Update color uniform project to color palette

--- Assignments/Assignment-1/ColorUniform/fragment.glsl
+++
@@ -1,6 +0,0 @@
-uniform vec3 colorUniform;     // vec3 - each coordinate for RED, GREEN and BLUE
-
-void main() {
-    // Start from BLACK and intensify/nullify from the input 
-    gl_FragColor = vec4(colorUniform.x, colorUniform.y, colorUniform.z, 1.0) + vec4(0.0, 0.0, 0.0, 1.0);
-}
